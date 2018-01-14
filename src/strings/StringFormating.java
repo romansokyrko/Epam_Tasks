b532ccabc104ff09d9ea6c59ec7989936b019887
Roman Sokyrko <romanacyl@gmail.com>
2018-01-14 17:19:09 +0200

    split into methods, finish testing

--- src/strings/StringFormating.java
+++ src/strings/StringFormating.java
@@ -9,52 +9,63 @@ public class StringFormating {
 	
 
 	public static void main(String[] args) {
-		int lineSize = 10;
+		int lineSize = 20;
 		Scanner in = new Scanner(System.in);
 		String inputSrting = null;
 		StringBuilder sb = new StringBuilder("");
 		while(in.hasNextLine()) {
 			inputSrting = in.nextLine();
 //			if(inputSrting.isEmpty()) break;
-			sb = sb.append("\n" +inputSrting);
+			sb = sb.append(inputSrting+ "\n");
 		}
 		in.close();
-		
-		// TODO: split into methods, finish testing
-		List<String> lines = new ArrayList<String>();
-		int beginIndx=0;
-		int endIndx=lineSize-1;
-		while(beginIndx<sb.length()) {
-			
-			if(endIndx>=sb.length()) {
-				endIndx=sb.length();
-			}
-			else if(sb.charAt(endIndx)!=' ') {
-				while(sb.charAt(endIndx-1)!=' ') {
-					endIndx--;
-					if(endIndx==beginIndx) {
-						endIndx=beginIndx+lineSize-1;
-						break;
-					}
-				}
-			}
-			if(beginIndx>=sb.length()-1) break;
-			String line = sb.substring(beginIndx, endIndx);
-			StringBuilder lineBuilder = new StringBuilder(line.trim());
-			
-			int spaces = lineSize-(lineBuilder.length());
-			for(int i =0; i<=spaces; i++) {
-				lineBuilder.insert(0, ' ');
-			}
-			lines.add(lineBuilder.toString());
-			beginIndx = endIndx;
-			endIndx=lineSize+beginIndx;
-			
-		}
-		
+		 
+		List<String> lines = rightAlignment(sb.toString(), lineSize);
+				
 		for(String line: lines) {
 			System.out.println(line);
 		}
+	} 
+	
+	public static List<String> rightAlignment(String text, int lineSize){
+		String[] inputLines = text.split("\n");
+		List<String> lines = new ArrayList<String>();
+		for(String inputLine: inputLines) {
+			int beginIndx=0;
+			int endIndx=lineSize;
+			while(beginIndx<inputLine.length()) { 
+				if(endIndx>=inputLine.length()) {
+					endIndx=inputLine.length();
+				}
+				else if(inputLine.charAt(endIndx)!=' ') {
+					while(inputLine.charAt(endIndx-1)!=' ') {
+						endIndx--;
+						if(endIndx==beginIndx) {
+							endIndx=beginIndx+lineSize;
+							break;
+						}
+					}
+				} 
+				if(beginIndx>=inputLine.length()-1) break;
+				String line = inputLine.substring(beginIndx, endIndx);
+				StringBuilder lineBuilder = new StringBuilder(line.trim());
+				
+				lineBuilder = fillLineWithSpaces(lineBuilder, lineSize);
+				lines.add(lineBuilder.toString());
+				beginIndx = endIndx;
+				endIndx=lineSize+beginIndx;
+				
+			}
+		}
+		return lines;
 	}
-
+	
+	public static StringBuilder fillLineWithSpaces(StringBuilder sb, int lineSize) {
+		int spaces = lineSize-(sb.length());
+		for(int i =0; i<=spaces-1; i++) {
+			sb.insert(0, ' ');
+		}
+		return sb;
+	}
+   
 }
