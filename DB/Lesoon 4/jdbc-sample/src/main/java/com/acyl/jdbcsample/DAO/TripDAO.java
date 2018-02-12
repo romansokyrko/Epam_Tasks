55b70d3ff889a610813f8d4b80c3e78552b859d8
Roman Sokyrko <romanacyl@gmail.com>
2018-02-12 06:30:09 +0200

    delete files

--- DB/Lesoon 4/jdbc-sample/src/main/java/com/acyl/jdbcsample/DAO/TripDAO.java
+++
@@ -1,7 +0,0 @@
-package com.acyl.jdbcsample.DAO;
-
-import com.acyl.jdbcsample.model.TripEntity;
-
-public interface TripDAO extends GeneralDAO<TripEntity, Integer>{
-
-}
--- DB/Lesoon 4/jdbc-sample/src/main/java/com/acyl/jdbcsample/ViewAndController/Printable.java
+++
@@ -1,8 +0,0 @@
-package com.acyl.jdbcsample.ViewAndController;
-
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface Printable {
-	void print() throws SQLException;
-}
